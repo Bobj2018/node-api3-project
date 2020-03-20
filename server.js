39418b1316b5fac74afa2ebc432fe1cce48cca81
Joshua Rieth <joshua.m.rieth@gmail.com>
2020-03-19 22:23:18 -0400

    add cors and serve-favicon

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require("cors")
+const favicon = require('serve-favicon')
 const postRouter = require('./posts/postRouter');
 const userRouter = require('./users/userRouter')
 
@@ -6,6 +8,9 @@ const server = express();
 
 
 server.use(express.json())
+server.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+server.use(cors())
+
 
 server.use(logger)
 server.use('/api/users', userRouter)
@@ -13,6 +18,10 @@ server.use('/api/posts', postRouter)
 server.get('/', (req, res) => {
   res.send('<h2>Let\'s write some middleware!</h2>');
 });
+
+server.get("/", (req, res) => {
+  res.status(200).send(<h1>Welcome to the API</h1>)
+})
 
 // custom middleware
 
